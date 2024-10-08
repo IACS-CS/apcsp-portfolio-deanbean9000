82be50cfa1e1255ff2949268aa6835fe4fed3832
deanbean9000 <ryan.dean@innovationcharter.org>
2024-10-08 14:57:17 +0000

    10/8 rd

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,8 @@
 import "../styles/styles.css";
 // Add imports here
-
+import "@picocss/pico/css/pico.min.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
 console.log("What?");
 console.log("Hello world, from main.js!");
 if (typeof window !== "undefined") {
@@ -9,4 +11,5 @@ if (typeof window !== "undefined") {
   //
   // If you see a SSR error, try moving your code here!
   console.log("Hello world, browser :-)");
+  Aos.inity();
 }
